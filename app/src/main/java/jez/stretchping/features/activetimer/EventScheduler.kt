0ed722c6d1b4dd76fd6dd4742ba25a9f39e8e425
Jez <5053926+Ninjars@users.noreply.github.com>
2023-04-04 22:56:50 +0100

    integrated sounds as part of EventScheduler's command handling

--- app/src/main/java/jez/stretchping/features/activetimer/EventScheduler.kt
+++ app/src/main/java/jez/stretchping/features/activetimer/EventScheduler.kt
@@ -1,11 +1,14 @@
 package jez.stretchping.features.activetimer
 
 import androidx.core.util.Consumer
+import jez.stretchping.audio.GameSoundEffect
 import jez.stretchping.audio.SoundManager
+import jez.stretchping.features.activetimer.ActiveTimerVM.State.SegmentSpec.Mode
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import java.lang.Integer.min
 import javax.inject.Inject
 import kotlin.time.Duration.Companion.seconds
 
@@ -22,17 +25,22 @@ class EventScheduler @Inject constructor(
         when (executedCommand) {
             is ActiveTimerVM.Command.EnqueueSegments -> Unit
             is ActiveTimerVM.Command.PauseSegment,
-            is ActiveTimerVM.Command.ResetToStart -> clearAllJobs()
+            is ActiveTimerVM.Command.ResetToStart -> {
+                clearAllJobs()
+                soundManager.playSound(GameSoundEffect.Stop)
+            }
             is ActiveTimerVM.Command.ResumeSegment ->
-                scheduleNextSegmentEvent(
+                scheduleNextSegmentEvents(
                     coroutineScope,
                     (executedCommand.remainingDurationMillis / 1000).toInt(),
+                    executedCommand.pausedSegment.mode,
                     eventConsumer,
                 )
             is ActiveTimerVM.Command.StartSegment ->
-                scheduleNextSegmentEvent(
+                scheduleNextSegmentEvents(
                     coroutineScope,
                     executedCommand.segmentSpec.durationSeconds,
+                    executedCommand.segmentSpec.mode,
                     eventConsumer,
                 )
         }
@@ -43,17 +51,49 @@ class EventScheduler @Inject constructor(
         jobs.clear()
     }
 
-    private suspend fun scheduleNextSegmentEvent(
+    private suspend fun scheduleNextSegmentEvents(
         coroutineScope: CoroutineScope,
         seconds: Int,
+        segmentMode: Mode,
         eventConsumer: Consumer<ActiveTimerVM.Event>,
     ) {
         jobs.add(
             coroutineScope.launch {
                 delay(seconds.seconds)
                 eventConsumer.accept(ActiveTimerVM.Event.OnSectionCompleted)
+                soundManager.playSound(
+                    when (segmentMode) {
+                        Mode.Stretch -> GameSoundEffect.ActiveSection
+                        Mode.Transition -> GameSoundEffect.TransitionSection
+                    }
+                )
             }
         )
+        enqueueCountdownPings(
+            coroutineScope = coroutineScope,
+            durationSeconds = seconds,
+            pingCount = 5,
+            pingInterval = when (segmentMode) {
+                Mode.Stretch -> 2
+                Mode.Transition -> 1
+            },
+        )
+    }
+
+    private suspend fun enqueueCountdownPings(
+        coroutineScope: CoroutineScope,
+        durationSeconds: Int,
+        pingCount: Int,
+        pingInterval: Int,
+    ) {
+        (1..(min(pingCount, durationSeconds / pingInterval))).forEach {
+            jobs.add(
+                coroutineScope.launch {
+                    delay((durationSeconds - it * pingInterval).seconds)
+                    soundManager.playSound(GameSoundEffect.CountdownBeep)
+                }
+            )
+        }
     }
 
     fun dispose() {

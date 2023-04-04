144ab4baf5a9d5942b1b73d7bb42bb57af2f97a2
Jez <5053926+Ninjars@users.noreply.github.com>
2023-04-04 22:56:06 +0100

    added sounds

--- app/src/main/java/jez/stretchping/audio/GameSoundEffect.kt
+++ app/src/main/java/jez/stretchping/audio/GameSoundEffect.kt
@@ -4,11 +4,19 @@ import android.content.Context
 import android.media.AudioAttributes
 import android.media.AudioAttributes.USAGE_GAME
 import android.media.SoundPool
+import jez.stretchping.R
+import jez.stretchping.audio.GameSoundEffect.ActiveSection
+import jez.stretchping.audio.GameSoundEffect.Back
+import jez.stretchping.audio.GameSoundEffect.CountdownBeep
+import jez.stretchping.audio.GameSoundEffect.Stop
+import jez.stretchping.audio.GameSoundEffect.TransitionSection
+import jez.stretchping.audio.GameSoundEffect.values
 
 enum class GameSoundEffect {
     Back,
-    StartActiveSection,
-    StartTransitionSection,
+    Stop,
+    ActiveSection,
+    TransitionSection,
     CountdownBeep,
 }
 
@@ -22,17 +30,17 @@ class GameSoundEffectPlayer {
             .setAudioAttributes(AudioAttributes.Builder().setUsage(USAGE_GAME).build())
             .build()
 
-//        soundIds = GameSoundEffect.values()
-//            .map {
-//                when (it) {
-//                    GameSoundEffect.Back -> TODO()
-//                    GameSoundEffect.StartActiveSection -> TODO()
-//                    GameSoundEffect.StartTransitionSection -> TODO()
-//                    GameSoundEffect.CountdownBeep -> TODO()
-//                }
-//            }.map {
-//                soundPool.load(context, it, 1)
-//            }
+        soundIds = values()
+            .map {
+                when (it) {
+                    Back, Stop -> R.raw.puzzle_success_xylophone_2_two_note_decline_low_wet_stereo
+                    ActiveSection -> R.raw.puzzle_success_xylophone_2_stab_wet_stereo
+                    TransitionSection -> R.raw.puzzle_success_xylophone_2_two_note_climb_bright_wet_stereo
+                    CountdownBeep -> R.raw.puzzle_success_xylophone_2_one_note_wet_stereo
+                }
+            }.map {
+                soundPool.load(context, it, 1)
+            }
     }
 
     fun tearDown() {

d1d99c8838babd1616d13bbc818a1eadfdb607d2
jez <5053926+Ninjars@users.noreply.github.com>
2024-07-27 00:15:42 +0100

    bottom-pad planner to provide overscroll space, to get content out from behind buttons

--- app/src/main/java/jez/stretchping/features/planner/PlannerScreen.kt
+++ app/src/main/java/jez/stretchping/features/planner/PlannerScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -167,6 +168,9 @@ private fun PopulatedContent(
             contentType = { "content" }
         ) {
             PlanSectionView(it, eventHandler)
+        }
+        item(key = "footer", contentType = "footer") {
+            Spacer(modifier = Modifier.height(112.dp))
         }
     }
 }
